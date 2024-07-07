fc6d58cb51d09bb75a12739a1589af71b02688d3
Matilde Enevoldsen <matildeene@outlook.com>
2024-07-07 21:07:28 +0100

    WIP

--- resources/views/livewire/pages/home.blade.php
+++ resources/views/livewire/pages/home.blade.php
@@ -31,7 +31,7 @@
                         <div class="mt-5">
                             <!-- Include the WireComments component -->
                             <livewire:comments allow-guests :emojis="['👍', '👎', '❤️', '😂', '😯', '😢', '😡']"
-                                               :model="App\Models\DocPage::find(2)"/>
+                                               :model="App\Models\DocPage::find(1)"/>
                         </div>
                     </div>
                 </div>
